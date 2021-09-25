d592d036066fa4ed33bc7c6582da6bc3612c3799
surbhi84 <subuhikukreti@gmail.com>
2021-09-25 16:29:35 +0530

    feat : data swap done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
+import { useState } from "react";
 import "./App.css";
 import { data } from "./data/data";
 function App() {
   const categories = ["Horror", "Comedy", "Romantic", "Fiction", "Action"];
+  const [currentCategory, setCurrentCategory] = useState();
   return (
     <div className="App">
       <header>Watch List Recommendation</header>
       <section>
         Category:{" "}
         {categories.map((category) => {
-          return <span> {category}</span>;
+          return (
+            <span
+              onClick={() => {
+                setCurrentCategory(category);
+              }}
+            >
+              {" "}
+              {category}
+            </span>
+          );
         })}
       </section>
-      <section>
-        {data["Horror"].map((rec) => (
-          <div>
-            <div>{rec.title}</div>
-            <span>{rec.description}</span>
-            <span>{rec.rating}</span>
-          </div>
-        ))}
-      </section>
+      {currentCategory && (
+        <section>
+          {data[currentCategory].map((rec) => (
+            <div>
+              <div>{rec.title}</div>
+              <span>{rec.description}</span>
+              <span>{rec.rating}</span>
+            </div>
+          ))}
+        </section>
+      )}
     </div>
   );
 }
