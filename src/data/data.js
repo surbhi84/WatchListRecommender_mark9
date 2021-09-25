66263995c7510a9e13d159a9ee28095ce6097e48
surbhi84 <subuhikukreti@gmail.com>
2021-09-25 16:12:37 +0530

    chore: dummy data

---
+++ src/data/data.js
@@ -0,0 +1,22 @@
+export const data = {
+  Horror: [
+    { title: "title Horror 0", description: "desc Horror 0", rating: 9 },
+    { title: "title Horror 1", description: "desc Horror 1", rating: 9 },
+  ],
+  Comedy: [
+    { title: "title Comedy 0", description: "desc Comedy 0", rating: 9 },
+    { title: "title Comedy 1", description: "desc Comedy 1", rating: 9 },
+  ],
+  Romantic: [
+    { title: "title Romantic 0", description: "desc Romantic 0", rating: 9 },
+    { title: "title Romantic 1", description: "desc Romantic 1", rating: 9 },
+  ],
+  Fiction: [
+    { title: "title Fiction 0", description: "desc Fiction 0", rating: 9 },
+    { title: "title Fiction 1", description: "desc Fiction 1", rating: 9 },
+  ],
+  Action: [
+    { title: "title Action 0", description: "desc Action 0", rating: 9 },
+    { title: "title Action 1", description: "desc Action 1", rating: 9 },
+  ],
+};
