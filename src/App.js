639931dd4b4b0d8311d5ad772b9f98660fadecde
surbhi84 <subuhikukreti@gmail.com>
2021-09-25 15:43:17 +0530

    feat : category added

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const categories = ["Horror", "Comedy", "Romantic", "Fiction", "Action"];
   return (
     <div className="App">
       <header>Watch List Recommendation</header>
-      <section>Category:</section>
+      <section>
+        Category:{" "}
+        {categories.map((category) => {
+          return <span> {category}</span>;
+        })}
+      </section>
       <section></section>
     </div>
   );
